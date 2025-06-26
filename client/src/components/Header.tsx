ecdc285ef984305088d3b1a24c62dd6657a0ef6b
David <bensadondavidn@gmai.com>
2025-06-27 02:15:43 +0300

    changes on the navbar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,18 +3,58 @@ import { SigninIcon } from "../assets/signInIcon"
 import { LoopIcon } from "../assets/loopIcon"
 import { HeartIcon } from "../assets/heartIcon"
 import { Cart4LineDuotoneIcon } from "../assets/cartIcon"
+import { useState } from "react"
 
 function Header() {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const handleOpen = () : void =>{
+    setIsOpen(prev=> !prev)
+  }
 
   return (
     <header>
       <div className="header-row">
-        <Link to='/signin'><SigninIcon />Sign In</Link>
-        <LoopIcon />
-        <Link to={'/likes'}><HeartIcon /></Link>
-        <Link to={'/cart'}><Cart4LineDuotoneIcon /></Link>
+
+        <div className="left-side">
+          <button className={`hamburger-button ${isOpen ? 'open' : ''}`} onClick={handleOpen} >
+              <span className="top-bar"></span>
+              <span className="bottom-bar"></span>
+          </button>
+          <Link to='/signin'><SigninIcon />Sign In</Link>
+        </div>
+
+        <p className="title">LEƎD BEAUTY</p>
+
+        <div className="right-side">
+          <LoopIcon />
+          <Link to={'/likes'}><HeartIcon className='likes' /></Link>
+          <Link to={'/cart'}><Cart4LineDuotoneIcon /></Link>
+        </div>
+
       </div>
-      <nav>
+
+      <hr style={{width : "100vw"}}/>
+
+      <nav className={isOpen ? 'open' : ''}>
+
+        <ul>
+          <li>
+            <Link></Link>
+          </li>
+          <li>
+            <Link></Link>
+          </li>
+          <li>
+            <Link></Link>
+          </li>
+          <li>
+            <Link></Link>
+          </li>
+          <li>
+            <Link></Link>
+          </li>
+        </ul>
 
       </nav>
     </header>
